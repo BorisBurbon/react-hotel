c211e37c6fd6712a9bcd0ed93ff5852f2f7ff59d
Mileev Boris <boris.burbon84@gmail.com>
2018-04-03 15:33:08 +0300

    fix bugs npm run

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ registerServiceWorker();
 // Test Redux
 
 
-function Hotel(state = [], action) {
-  if(action.type === "Add_hotel"){
-    return [...state, action.payload]
-  }
-  return state;
-}
-const store = createStore(Hotel);
-
-store.subscribe(() => {
-  console.log('subscribe', store.getState())
-});
-
-store.dispatch({type: "Add_hotel", payload: 'hotel1'});
-store.dispatch({type: "Add_hotel", payload: 'hotel2'});
+// function Hotel(state = [], action) {
+//   if(action.type === "Add_hotel"){
+//     return [...state, action.payload]
+//   }
+//   return state;
+// }
+// const store = createStore(Hotel);
+//
+// store.subscribe(() => {
+//   console.log('subscribe', store.getState())
+// });
+//
+// store.dispatch({type: "Add_hotel", payload: 'hotel1'});
+// store.dispatch({type: "Add_hotel", payload: 'hotel2'});
