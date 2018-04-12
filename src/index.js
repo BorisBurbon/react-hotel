79b003e594a8489adddc9699055d0409dca0c9c3
Mileev Boris <boris.burbon84@gmail.com>
2018-04-12 12:32:38 +0300

    add routing

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import './index.css';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {Router, Route, browserHistory} from 'react-router';
+
 import App from './App';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+import './index.css';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
