506f1d79f9a1e96e4e0152aede49d37775173cb9
Mileev Boris <boris.burbon84@gmail.com>
2018-04-13 14:03:07 +0300

    add map

--- src/listhotel/AboutHotel.js
+++ src/listhotel/AboutHotel.js
@@ -2,22 +2,34 @@ import {connect} from 'react-redux';
 import React, {Component} from 'react';
 import Header from '../header/header.js';
 import _ from 'lodash'
+import GoogleMapReact from 'google-map-react'
+
+import  './listhotel.css'
+
+const AnyReactComponent = ({ text }) => <div className="google-icons">{ text }</div>;
 
 
 class AboutHotel extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       property_name: ''
     }
-
   }
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 14
+  };
+
 
 
   render() {
     let hotel = _.head(_.filter(this.props.hotelList.hotel, { 'property_code': this.props.match.params.id}));
     console.log(hotel);
+
 
       return (
         <div className="about-hotel">
@@ -33,6 +45,20 @@ class AboutHotel extends Component {
                   return  <li key={key}>{item.amenity}</li>;
                 })
               }
+              <div style={{ height: '400px', width: '100%' }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: "AIzaSyBJ2n-pBl_-IRzhGA7MTpIG3udWuY1DM7k" }}
+                  center = {{ lat: hotel.location.latitude, lng: hotel.location.longitude }}
+                  defaultZoom={this.props.zoom}
+                >
+
+                  <AnyReactComponent
+                    lat={hotel.location.latitude}
+                    lng={hotel.location.longitude}
+                    text={''}
+                  />
+                </GoogleMapReact>
+              </div>
             </div>
           }
         </div>
